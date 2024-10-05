cb710c0f7486ee49b6ede8fcc0666551baea621e
Mayank Verma <mayankverma.dev@gmail.com>
2024-10-05 15:42:00 +0530

    added new Project apply dummy clone

--- src/Component/Project/Project.jsx
+++ src/Component/Project/Project.jsx
@@ -7,7 +7,7 @@ let projectList = [
     projectTitle:'Notes App',
     projectDetails:'Note-taking app with canvas and quill editor for dynamic, rich text and visual note creation.',
     liveUrl:'https://notesbymayankverma.netlify.app/',
-    githubUrl:'',
+    githubUrl:'https://github.com/Wannacryy24/SuperNotes',
     imageSrc:'/notesSS.png',
     technologiesUsed:['Html5','Css3','Javascript','Quill Js','Canva Js','Local Storage','Vite'],
   },
@@ -15,7 +15,7 @@ let projectList = [
     projectTitle:'Airbnb Clone',
     projectDetails:'Airbnb with infinite scrolling and showcases detailed property information.',
     liveUrl:'https://abodehome.netlify.app/',
-    githubUrl:'',
+    githubUrl:'https://github.com/Wannacryy24/pgFinder',
     imageSrc:'/air.png',
     technologiesUsed:['React', 'Html5','Css3','Javascript', 'MirageJs' ,'Infinite Scroll' , 'Ajax' , 'Json'],
   },
@@ -23,7 +23,7 @@ let projectList = [
     projectTitle:'Quiz Maker',
     projectDetails:'Quiz-Maker app for creating, previewing, and saving quizzes with local storage persistence.',
     liveUrl:'https://quizzmaker.netlify.app/',
-    githubUrl:'',
+    githubUrl:'https://github.com/Wannacryy24/QuizMaker',
     imageSrc:'/Quiz.png',
     technologiesUsed:['Javascript','Html5','Css3','Local Storage','Ajax','Json'],
     },
@@ -31,7 +31,7 @@ let projectList = [
       projectTitle:'Trivia Quiz',
       projectDetails:'Quiz app featuring interactive quizzes with a countdown timer and Final Result.',
       liveUrl:'https://quizappbymayankverma.netlify.app/',
-      githubUrl:'',
+      githubUrl:'https://github.com/Wannacryy24/QuizApp',
       imageSrc:'/triviaQuiz.png',
       technologiesUsed:['Html5','Css3','Javascript','Vite','Json'],
     },
@@ -41,8 +41,16 @@ let projectList = [
       liveUrl:'https://mayankvermaportfolio.netlify.app/',
       githubUrl:'',
       imageSrc:'/jsPortfolio.png',
-      technologiesUsed:['Html5','Css3','Javascript','vite'],
+      technologiesUsed:['Html5','Css3','Javascript'],
     },
+    {
+      projectTitle:'Apple Clone Dummy',
+      projectDetails:'Apple Clone Dummy with responsiveness using html , css , javascript, React, JSON',
+      liveUrl:'https://iclonerreact.netlify.app/',
+      githubUrl:'https://github.com/Wannacryy24/appleCloner',
+      imageSrc:'/apple.png',
+      technologiesUsed:['Html5','Css3','Javascript','React'],
+    }
   ];
 
 export default function Project() {
