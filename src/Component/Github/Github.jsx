6a4fc5c4b156f56d7b7bf7f97e5230efdadb3010
Mayank Verma <mayankverma.dev@gmail.com>
2025-02-15 10:29:19 +0530

    added Updated Lead Resume

--- src/Component/Github/Github.jsx
+++ src/Component/Github/Github.jsx
@@ -72,7 +72,7 @@ export default function Github() {
           <span className='left-framgent-of-btn'>
           &lt;button&gt;
           </span>
-            <a href="/MayankVerma_Resume2025.pdf" target='_blank'>
+            <a href="/Mayank2025feb.pdf" target='_blank'>
               <span className='download-resume'>
               Download Resume
               </span>
